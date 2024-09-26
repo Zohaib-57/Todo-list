3fcb774fd94adeb53cdb9a14e7890e85edcd22c8
Your Name <zohaibabbas8557@gmail.com>
2024-09-26 08:54:10 +0500

    creating and appending delete button and other elements

--- script.js
+++ script.js
@@ -6,8 +6,20 @@ function addTask() {
         var taskList = document.getElementById("taskList");
         var li = document.createElement("li");
         li.textContent = taskText;
-    
+
+
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.className = "delete-button";
+
+        deleteButton.addEventListener("click", function() {
+            taskList.removeChild(li);
+        });
+
+        li.appendChild(deleteButton); 
+        taskList.appendChild(li);
+        taskInput.value = "";
+    } else {
+        alert("Please enter a task!");
     }
-
-
 }
