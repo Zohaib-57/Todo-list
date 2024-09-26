ad8681c42746cf72ed3f2c3a8f22ee5d9c391939
Your Name <zohaibabbas8557@gmail.com>
2024-09-26 08:50:57 +0500

    creating the list item using DOM

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function addTask() {
     var taskInput = document.getElementById("task-input");
     var taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        var taskList = document.getElementById("taskList");
+        var li = document.createElement("li");
+        li.textContent = taskText;
+    
+    }
+
+
 }
